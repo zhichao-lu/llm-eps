53162ab7a20ad441cf3a07e785f01963c671c49c
RayZhhh <hey_ray_zhang@icloud.com>
2024-04-25 17:57:29 +0800

    llm-eps

--- README.md
+++ README.md
@@ -1,15 +1,190 @@
-# Code for *Towards Understanding the Effectiveness of Automatic Heuristic Design with Large Language Models*
+# Code implementations for *FunSearch*, *RandomSampling*, and *(1+1)-EPS*
 
-## Installation and Requirements
+This repository includes code for the following paper:
+
+> *Towards Understanding the Effectiveness of Automatic Heuristic Design with Large Language Models*
+
+------
+
+## Installation and requirements
 
 Please note that **the Python version must be larger or equal to Python 3.9**, or the '*ast*' package used in the implementations will fail to work. 
 
-You can run FunSearch for online bin packing locally if enough GPU devices are available. Or you can try to use LLM interfaces to request responses online. 
+You can run  locally if enough GPU devices are available. Or you can try to use LLM interfaces to request responses online. 
 
 Please install the packages listed in `requirements.txt`.
 
-## Project Structure
+## LLMs
 
-## Run with Local LLM
+Not all LLMs can be used for an LLM-based EPS method.
 
-## Run with LLM API
+| LLMs                   | FunSearch | (1+1)-EPS (HillClimbing) | RandomSampling |
+| :--------------------- | :-------: | :----------------------: | :------------: |
+| UniXcoder              |     x     |            x             |       v        |
+| CodeLlama-7B-instruct  |     v     |            v             |       v        |
+| CodeLlama-34B-Instruct |     v     |            v             |       v        |
+| StarCoder              |     v     |            v             |       v        |
+| DeepSeek-Coder-6.7B    |     v     |            v             |       v        |
+| DeepSeek-Coder-33B     |     v     |            v             |       v        |
+| GPT-3.5 (API)          |     v     |            v             |       v        |
+| GPT-4 (API)            |     v     |            v             |       v        |
+| Claude-3-Opus (API)    |     v     |            v             |       v        |
+
+## Project structure
+
+**`funsearch`** folder includes FunSearch implementations.
+
+&nbsp;&nbsp;&nbsp;&nbsp;|----`task_name` (admissible_set, bin_packing_or, bin_packing_weibull, gls_tsp)
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|----`task_name_completion.py` for LLMs that support code completion generation (please refer to the table below).
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|----`task_name_instruct.py` for LLMs that support instruct generation (please refer to the table below).
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|----`task_name_api.py` using LLM api (please refer to the table below).
+
+&nbsp;&nbsp;&nbsp;&nbsp;|----`funsearch_impl`
+
+**`rand_search`** folder includes RandomSampling implementations.
+
+&nbsp;&nbsp;&nbsp;&nbsp;|----`task_name` (admissible_set, bin_packing_or, bin_packing_weibull, gls_tsp)
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|----`task_name_completion.py` for LLMs that support code completion generation (please refer to the table below).
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|----`task_name_instruct.py` for LLMs that support instruct generation (please refer to the table below).
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|----`task_name_api.py` using LLM api (please refer to the table below).
+
+&nbsp;&nbsp;&nbsp;&nbsp;|----`rand_search_impl`
+
+**`hill_climb`** folder includes (1+1)-EPS implementations.
+
+&nbsp;&nbsp;&nbsp;&nbsp;|----`task_name` (admissible_set, bin_packing_or, bin_packing_weibull, gls_tsp)
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|----`task_name_completion.py` for LLMs that support code completion generation (please refer to the table below).
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|----`task_name_instruct.py` for LLMs that support instruct generation (please refer to the table below).
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|----`task_name_api.py` using LLM api (please refer to the table below).
+
+&nbsp;&nbsp;&nbsp;&nbsp;|----`hill_climb_impl`
+
+## Run LLM-based EPS
+
+### Python script
+
+Run differnet python script for different LLM.
+
+| LLMs                   | Run Python file         |
+| :--------------------- | :---------------------- |
+| UniXcoder              | task_name_completion.py |
+| CodeLlama-7B-instruct  | task_name_completion.py |
+| CodeLlama-34B-Instruct | task_name_completion.py |
+| StarCoder              | task_name_completion.py |
+| DeepSeek-Coder-6.7B    | task_name_instruct.py   |
+| DeepSeek-Coder-33B     | task_name_instruct.py   |
+| GPT-3.5 (API)          | task_name_api.py        |
+| GPT-4 (API)            | task_name_api.py        |
+| Claude-3-Opus (API)    | task_name_api.py        |
+
+### Run with local LLM
+
+##### Download LLM
+
+Please refer to tutorials at Huggingface (https://huggingface.co) or Huggingface mirror  (https://hf-mirror.com).
+
+##### Deploy an LLM locally
+
+Please note that it is suggested to deploy the LLM server in a 'screen' environment. Which can be created as below:
+
+```shee
+screen -R local_llm
+```
+
+The server scripts are in `llm_server` folder. Please find the right python file.
+
+```shell
+cd llm_server
+
+python xxx.py \
+	--path [your path to LLM] \
+  --d [GPU ids, e.g., 0 1 2 3 4] \
+  --host [the ip of the server, default is 127.0.0.1] \
+  --port [the port of the server] \
+  --quantization  # if you want to do quantization, please add this argument
+```
+
+##### Prepare a config file
+
+If there is an issue with the LLM server, or you need to change the url of the LLM during **runtime**, you can simply modify the config file instead of re-running.
+
+An template for a config file is shown below:
+
+```json
+{
+  "url": "http://127.0.0.1:11101/completions",  # the URL for the LLM server
+  "samples_per_prompt": 4,                      # samples per prompt
+  "batch_inference": true                       # whether to use batch inference to accelerate
+}
+```
+
+##### Start LLM-based EPS 
+
+We suggest run using 'nohup'. The instruction is shown below.
+
+```shell
+nohup python xxx.py --run 1 --config run1_runtime_llm_config.json > run1.out 2>&1 &
+```
+
+### Run with LLM API
+
+##### Choose the correct LLM model and set the API key
+
+Please modify the code of `_draw_sample` function in `task_name_api.py`. 
+
+```python
+def _draw_sample(self, content: str) -> str:
+    prompt = '\n'.join([content, self._additional_prompt])
+    while True:
+        try:
+            conn = http.client.HTTPSConnection(
+              "api.chatanywhere.com.cn",  # 1. Set API URL.
+              timeout=self._timeout
+            )  
+            payload = json.dumps({
+                "max_tokens": 512,
+                "model": "gpt-3.5-turbo",  # 2. Set LLM model name. Please refer to your API provider.
+                "messages": [
+                    {
+                        "role": "user",
+                        "content": prompt
+                    }
+                ]
+            })
+            headers = {
+                'Authorization': 'Bearer sk-your api key here',  # 3. Set your API key.
+                'User-Agent': 'Apifox/1.0.0 (https://apifox.com)',
+                'Content-Type': 'application/json'
+            }
+            conn.request("POST", "/v1/chat/completions", payload, headers)
+            res = conn.getresponse()
+            data = res.read().decode("utf-8")
+            data = json.loads(data)
+            response = data['choices'][0]['message']['content']
+            # trim function
+            if self._trim:
+                response = _trim_preface_of_body(response)
+            return response
+        except Exception as e:
+            # print(e)
+            time.sleep(2)
+            continue
+```
+
+##### Start LLM-based EPS
+
+We suggest run using 'nohup'. The instruction is shown below.
+
+```shell
+nohup python xxx.py --run 1 > run1.out 2>&1 &
+```
+
---
+++ funsearch/bin_packing_weibull/bin_packing_weibull_local_completion.py
@@ -0,0 +1,226 @@
+import json
+import multiprocessing
+from argparse import ArgumentParser
+from typing import Collection, Any, Tuple
+import pickle
+import requests
+import sys
+
+sys.path.append('../')
+from bin_packing_weibull import utils
+from funsearch_impl import funsearch
+from funsearch_impl import config
+from funsearch_impl import sampler
+from funsearch_impl import evaluator_accelerate
+from funsearch_impl import evaluator
+from funsearch_impl import code_manipulation
+
+parser = ArgumentParser()
+parser.add_argument('--run', type=int)
+parser.add_argument('--config', type=str, default='run1_runtime_config.json')
+parser.add_argument('--resume_run', default=False, action='store_true')
+args = parser.parse_args()
+
+
+def _load_runtime_config():
+    config_path = args.config
+    assert config_path is not None
+    with open(config_path, 'r') as file:
+        data_dict = json.load(file)
+    return data_dict
+
+
+class LocalLLM(sampler.LLM):
+    """Language model that predicts continuation of provided source code.
+    """
+
+    def _update_config(self):
+        try:
+            config_dict = _load_runtime_config()
+            if config_dict['url'] is None:
+                self._url = self._default_url
+            else:
+                self._url = config_dict['url']
+
+            if config_dict['batch_inference'] is None:
+                self._batch_inference = self._default_batch_inference
+            else:
+                self._batch_inference = config_dict['batch_inference']
+
+            if config_dict['samples_per_prompt'] is None:
+                self._samples_per_prompt = self._default_samples_per_prompt
+            else:
+                self._samples_per_prompt = config_dict['samples_per_prompt']
+        except:
+            self._url = self._default_url
+            self._batch_inference = self._default_batch_inference
+            self._samples_per_prompt = self._default_samples_per_prompt
+
+    def __init__(
+            self,
+            samples_per_prompt: int,
+            batch_inference: bool = True
+    ) -> None:
+        """
+        Args:
+            batch_inference: Use batch inference when sample functions. The batch size equals to the samples_per_prompt.
+        """
+        super().__init__(samples_per_prompt)
+        self._default_url = 'http://127.0.0.1:11045/completions'
+        self._default_samples_per_prompt = samples_per_prompt
+        self._default_batch_inference = batch_inference
+        #
+        self._url = self._default_url
+        self._batch_inference = self._default_batch_inference
+
+    def draw_samples(self, prompt: str) -> Collection[str]:
+        """Returns multiple predicted continuations of `prompt`.
+        """
+        while True:
+            try:
+                self._update_config()
+                all_samples = []
+                if self._batch_inference:
+                    response = self._do_request(prompt)
+                    for res in response:
+                        all_samples.append(res)
+                else:
+                    for _ in range(self._samples_per_prompt):
+                        response = self._do_request(prompt)
+                        all_samples.append(response)
+                return all_samples
+            except:
+                continue
+
+    def _do_request(self, content: str) -> str:
+        content = content.strip('\n').strip()
+        # repeat the prompt for batch inference (inorder to decease the sample delay)
+        repeat_prompt: int = self._samples_per_prompt if self._batch_inference else 1
+        data = {
+            'prompt': content,
+            'repeat_prompt': repeat_prompt,
+            'params': {
+                'do_sample': True,
+                'temperature': None,
+                'top_k': None,
+                'top_p': None,
+                'add_special_tokens': False,
+                'skip_special_tokens': True,
+            }
+        }
+        headers = {'Content-Type': 'application/json'}
+        response = requests.post(self._url, data=json.dumps(data), headers=headers)
+        if response.status_code == 200:
+            response = response.json()["content"]
+            return response if self._batch_inference else response[0]
+
+
+class Sandbox(evaluator.Sandbox):
+    """Sandbox for executing generated code. Implemented by RZ.
+
+    RZ: Sandbox returns the 'score' of the program and:
+    1) avoids the generated code to be harmful (accessing the internet, take up too much RAM).
+    2) stops the execution of the code in time (avoid endless loop).
+    """
+
+    def __init__(self, verbose=False, numba_accelerate=True):
+        """
+        Args:
+            verbose         : Print evaluate information.
+            numba_accelerate: Use numba to accelerate the evaluation. It should be noted that not all numpy functions
+                              support numba acceleration, such as np.piecewise().
+        """
+        self._verbose = verbose
+        self._numba_accelerate = numba_accelerate
+
+    def run(
+            self,
+            program: str,
+            function_to_run: str,  # RZ: refers to the name of the function to run (e.g., 'evaluate')
+            function_to_evolve: str,  # RZ: accelerate the code by decorating @numba.jit() on function_to_evolve.
+            inputs: Any,  # refers to the dataset
+            test_input: str,  # refers to the current instance
+            timeout_seconds: int,
+            # **kwargs  # RZ: add this
+    ) -> tuple[Any, bool]:
+        """Returns `function_to_run(test_input)` and whether execution succeeded.
+
+        RZ: If the generated code (generated by LLM) is executed successfully,
+        the output of this function is the score of a given program.
+        RZ: PLEASE NOTE THAT this SandBox is only designed for bin-packing problem.
+        """
+        dataset = inputs[test_input]
+        try:
+            result_queue = multiprocessing.Queue()
+            process = multiprocessing.Process(
+                target=self._compile_and_run_function,
+                args=(program, function_to_run, function_to_evolve, dataset, self._numba_accelerate, result_queue)
+            )
+            process.start()
+            process.join(timeout=timeout_seconds)
+            if process.is_alive():
+                # if the process is not finished in time, we consider the program illegal
+                process.terminate()
+                process.join()
+                results = None, False
+            else:
+                if not result_queue.empty():
+                    results = result_queue.get_nowait()
+                else:
+                    results = None, False
+
+            return results
+        except:
+            return None, False
+
+    def _compile_and_run_function(self, program, function_to_run, function_to_evolve, dataset, numba_accelerate,
+                                  result_queue):
+        try:
+            # optimize the code (decorate function_to_run with @numba.jit())
+            if numba_accelerate:
+                program = evaluator_accelerate.add_numba_decorator(
+                    program=program,
+                    function_name=function_to_evolve
+                )
+            # compile the program, and maps the global func/var/class name to its address
+            all_globals_namespace = {}
+            # execute the program, map func/var/class to global namespace
+            exec(program, all_globals_namespace)
+            # get the pointer of 'function_to_run'
+            function_to_run = all_globals_namespace[function_to_run]
+            # return the execution results
+            results = function_to_run(dataset)
+            # the results must be int or float
+            if not isinstance(results, (int, float)):
+                result_queue.put((None, False))
+                return
+            result_queue.put((results, True))
+        except:
+            # if raise any exception, we assume the execution failed
+            result_queue.put((None, False))
+
+
+# It should be noted that the if __name__ == '__main__' is required.
+# Because the inner code uses multiprocess evaluation.
+if __name__ == '__main__':
+    class_config = config.ClassConfig(llm_class=LocalLLM, sandbox_class=Sandbox)
+    config = config.Config(samples_per_prompt=4, evaluate_timeout_seconds=20)
+
+    # if it is set to None, funsearch will execute an endless loop
+    global_max_sample_num = 10_000
+
+    # load dataset
+    with open('../bin_packing_weibull_datasets/weibull_train.pkl', 'rb') as f:
+        bin_packing_weibull_train = pickle.load(f)
+
+    # bin_packing_weibull_train = {'weibull': utils.datasets['Weibull 5k']}
+
+    funsearch.main(
+        specification=utils.specification,
+        inputs=bin_packing_weibull_train,
+        config=config,
+        max_sample_nums=global_max_sample_num,
+        class_config=class_config,
+        log_dir=f'logs/run{args.run}',
+        resume_run=args.resume_run
+    )
